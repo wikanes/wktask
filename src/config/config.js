122150d8f56331b79a89d9f3714880bf002406b5
wikanes <44151516+wikanes@users.noreply.github.com>
2018-10-19 22:56:45 +0700

    Delete config.js

--- src/config/config.js
+++
@@ -1,8 +0,0 @@
-export const DB_CONFIG = {
-  apiKey: "AIzaSyA0hRpcFqyBLDmvNXy1AFUzK-Jm78Zg0Iw",
-  authDomain: "wikanotes.firebaseapp.com",
-  databaseURL: "https://wikanotes.firebaseio.com",
-  projectId: "wikanotes",
-  storageBucket: "wikanotes.appspot.com",
-  messagingSenderId: "447985225344"
-};
